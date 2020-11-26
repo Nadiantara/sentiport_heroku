11f16777cf9325d8d2abdb310e1db7585d6879ed
ardyh <13517062@std.stei.itb.ac.id>
2020-11-26 19:12:31 +0700

    added threading functionality: handled concurrent access & request timeout

--- sentiport/__init__.py
+++ sentiport/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_mail import Mail
+import threading
+
+thread_lock = threading.Lock()
+pipeline_thread = threading.Thread()
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
